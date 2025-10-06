5a7c3d02443434177a1db1bd848ed9dfd84adbdc
Avigail <29akravets@gannacademy.org>
2025-10-06 15:30:13 -0400

    Is this the way to save???

--- src/App.java
+++ src/App.java
@@ -3,5 +3,30 @@ import processing.core.*;
 public class App extends PApplet {
     public static void main(String[] args)  {
         PApplet.main("App");
+
+    }
+    public void settings() {
+        size(400, 400); 
+    }
+    public void setup() {
+       
+        background(255, 16, 240);
+
+        stroke(128, 0, 128); 
+        strokeWeight(21);
+        line(150, 400, 200, 100); 
+
+        
+
+         fill(207, 255, 4); 
+        stroke(57, 255, 20); 
+        strokeWeight(7);
+        rectMode(CENTER); // draw rectangle from its center
+        rect(width / 2, height / 2, 200, 100); // centered 200×100 rectangle
+
+        fill(57, 255, 20);
+        stroke(207, 255, 4); 
+        strokeWeight(12);
+        ellipse(100, 300, 170, 170);
     }
 }
